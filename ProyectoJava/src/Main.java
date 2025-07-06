1d4d36685671810de433ea95520f34bd866347b4
David Madrigal <dm5566694@gmail.com>
2025-07-06 14:31:08 -0600

    Add files via upload

--- ProyectoJava/src/Main.java
+++ ProyectoJava/src/Main.java
@@ -2,5 +2,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("prueba github");
+        System.out.println("prueba 2 github, verificar si se creo clase ");
     }
 }
