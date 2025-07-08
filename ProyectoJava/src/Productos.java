dc39bb05a7a36aba3edd58733ff4cd0fb0987be9
samuelgonzalezhernandez <Samuel.go.he1996@gmail.com>
2025-07-08 17:43:07 -0600

    Add files via upload

--- ProyectoJava/src/Productos.java
+++ ProyectoJava/src/Productos.java
@@ -7,12 +7,12 @@ public class Productos {
     // METODOS
 
     public void escogerDia() {
-        String[] paquetes = {"1 semana completa", "2  3 dias y 2 noches", "3 vip ", "4 Todas las anteriores +areas recreativas"};
-        float[] precios = {50000, 70000, 120000, 150000};
+        String[] paquetes = {"1 Semana completa", "2  3 Dias y 2 Noches", "3 Vip ", "4 Todas las anteriores +areas recreativas"};
 
 
 
-        JOptionPane.showMessageDialog(null, "Bienvenidos a ventas de paquetes, a continuacion se le precentaran 4 opciones de paquetes ");
+
+        JOptionPane.showMessageDialog(null, "Bienvenidos a paquetes. ");
         JOptionPane.showMessageDialog(null, "1= semana completa con valor de 50.000 " +
                 "\n" + "2= 3 dias y 2 noches con un valor de 70.000" + "\n"
                 + "3= vip con un valor de 120.000" + "\n" +
@@ -24,21 +24,46 @@ public class Productos {
         Facturacion fct = new Facturacion();
 
         if (opcion1 == 1) {
-            JOptionPane.showMessageDialog(null, (paquetes[0] + precios[0]));
+            JOptionPane.showMessageDialog(null, (paquetes[0]));
             sumaProductos+= Facturacion.semanaCompleta;
         } else if (opcion1 == 2) {
-            JOptionPane.showMessageDialog(null, (paquetes[1] + precios[1]));
+            JOptionPane.showMessageDialog(null, (paquetes[1]));
             sumaProductos+= Facturacion.dosNoches;
         } else if (opcion1 == 3) {
-            JOptionPane.showMessageDialog(null, (paquetes[2] + precios[2]));
+            JOptionPane.showMessageDialog(null, (paquetes[2]));
             sumaProductos= Facturacion.vip;
         } else if (opcion1 == 4) {
-            JOptionPane.showMessageDialog(null, (paquetes[3] + precios[3]));
+            JOptionPane.showMessageDialog(null, (paquetes[3]));
             sumaProductos+= Facturacion.recreativas;
         } else
             JOptionPane.showMessageDialog(null, "seleccion incorrecta");
 
         }
+
+
+
+    // crear adicinal
+   public void paquetesAdicionales(){
+
+        JOptionPane.showMessageDialog(null, "Tenemos paquetes adicionales para usted "
+                +"\n"+"1 camping"+"\n"+"2 senderismo");
+
+        int lectura1 = Integer.parseInt(JOptionPane.showInputDialog("elija el paquete deseado, o cualquier otro numero para salir "));
+        if (lectura1==1){
+            JOptionPane.showMessageDialog(null,"a seleccionado camping con exito");
+            sumaProductos+=Facturacion.camping;
+        }
+        else if (lectura1==2){
+            sumaProductos+=Facturacion.senderismo;
+        }
+        else JOptionPane.showMessageDialog(null,"no se escogieron adiccionales, gracias");
+    }
+
+
+
+
+
+
     }
 
 
